51dca405994f45c62d8e7ef77ced3040c7887ec1
Rronny01 <72240947+Rronny01@users.noreply.github.com>
2020-10-03 14:22:30 +0530

    Create re_for_sum.py

---
+++ Python/re_for_sum.py
@@ -0,0 +1,12 @@
+import re
+
+name = open('re_sum.txt')
+lst = list()
+for line in name :
+    num = re.findall('[0-9]+', line)
+    for i in num :
+        if len(i) >= 1 :
+            n = int(i)
+            lst.append(n)
+print(lst)
+print(sum(lst))
