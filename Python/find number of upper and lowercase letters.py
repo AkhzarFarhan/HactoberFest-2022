471481de83d2f50ba7cadfe887b41db0526e71de
Gunashree C <55580701+Guna-777@users.noreply.github.com>
2020-10-05 18:29:48 +0530

    Add files via upload

---
+++ Python/find number of upper and lowercase letters.py
@@ -0,0 +1,14 @@
+def string_test(s):
+    d={"UPPER_CASE":0, "LOWER_CASE":0}
+    for c in s:
+        if c.isupper():
+           d["UPPER_CASE"]+=1
+        elif c.islower():
+           d["LOWER_CASE"]+=1
+        else:
+           pass
+    print ("Original String : ", s)
+    print ("No. of Upper case characters : ", d["UPPER_CASE"])
+    print ("No. of Lower case Characters : ", d["LOWER_CASE"])
+
+string_test('GOOD Morning')
