9b65e2c05a231d28a416c9196922b166a6d0f7cd
Mayur Vora <mvora460@gmail.com>
2020-10-18 14:06:02 +0530

    brute force attack on given message

---
+++ Python/bruteforce.py
@@ -0,0 +1,14 @@
+message = 'PYTHON' 
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+for key in range(len(LETTERS)):
+    translated = ''
+    for symbol in message:
+        if symbol in LETTERS:
+            num = LETTERS.find(symbol)
+            num = num - key
+        if num<0:
+            num = num + len(LETTERS)
+            translated = translated + LETTERS[num]
+        else:
+            translated = translated + symbol
+    print('Hacking key #%s: %s' % (key, translated))
