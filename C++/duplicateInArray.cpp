eec9ec09763b1d11028982072f758f5f68a2b9fd
Jakaria <41715205+jakaria9001@users.noreply.github.com>
2020-10-23 20:08:00 +0530

    Create duplicateInArray.cpp

---
+++ C++/duplicateInArray.cpp
@@ -0,0 +1,19 @@
+//find duplicate: 
+// Link : https://leetcode.com/problems/contains-duplicate-ii/
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        unordered_map<int,int> m;
+        unordered_map<int,int>::iterator it;
+        
+        
+        for(int i=0; i<nums.size(); ++i) {
+            it = m.find(nums[i]);
+            if(it != m.end() and i - it->second <= k) return true;
+            else m[nums[i]] = i;
+        }
+        return false;
+    }
+};
