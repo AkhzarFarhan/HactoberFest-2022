4c63ab22195546cdbf174c2e9d8ca8c848695867
Ghatage <ghatageanup@gmail.com>
2020-10-23 09:44:22 -0700

    Add Length of longest substring problem in cpp folder

---
+++ C++/lengthOfLongestSubstring.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+    
+        unordered_map <char,int> ump;
+            
+        int mx=0,x=0;
+        for(int i=0;i<s.size();i++){
+            if(ump[s[i]]==0){
+                ump[s[i]]++;
+                x++;
+            }else{
+                mx = max(mx,x);
+                //back to (i-x) index for relooping from the double occurance
+                if(x>0) i-=x;
+                x=0;
+                ump.clear();
+            }
+        }
+        mx = max(x,mx);
+        return mx;
+        
+    }
+};
