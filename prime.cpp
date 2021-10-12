78cbb026a7d68fa4984a1876bfac4a24e4ceb694
MuskanTaya23 <92319160+MuskanTaya23@users.noreply.github.com>
2021-10-12 21:51:27 +0530

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n,div=2;
+  cout<<"Enter a number : "<<endl;
+  cin>>n;
+  for(div=2;div<n;div++)
+  {
+    if(n%div==0)
+    {
+      cout<<"Non Prime"<<endl;
+      break;
+    }
+    if(div==num)
+    {
+      cout<<"It is a Prime number"<<endl;
+    }
+  }
+}
+    
