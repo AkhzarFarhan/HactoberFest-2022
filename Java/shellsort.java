f6bf8b48b7d3fa6e8367871246ee373f4029a220
ins-arun <60340001+ins-arun@users.noreply.github.com>
2021-10-26 22:22:06 +0530

    Update shellsort.java

--- Java/shellsort.java
+++ Java/shellsort.java
@@ -1,38 +1,28 @@
-import java.util.*;  
-public class Shell_Sort   
-{  
-static void shellsort(int[] arr, intnum)    
-{    
-int i, j, k, tmp;    
-for (i = num / 2; i> 0; i = i / 2)    
-{    
-for (j = i; j<num; j++)    
-{    
-for(k = j - i; k>= 0; k = k - i)    
-{    
-if (arr[k+i] >= arr[k])    
-break;    
-else  
-{    
-tmp = arr[k];    
-arr[k] = arr[k+i];    
-arr[k+i] = tmp;    
-}    
-}    
-}    
-}    
-}     
-public static void main(String[] args)   
-{    
-Scanner sc = new Scanner(System.in);  
-int arr[]= newint[30];    
-intk,  num;    
-System.out.println("Enter total no. of elements : ");      
-num = sc.nextInt();  
-for (k =  0 ; k<num; k++)    
-{    
-arr[k]=sc.nextInt();    
-}    
+public class Demo {
+   int shell_sort(int my_arr[]) {
+      int arr_len = my_arr.length;
+      for (int gap = arr_len / 2; gap > 0; gap /= 2) {
+         for (int i = gap; i < arr_len; i += 1) {
+            int temp = my_arr[i];
+            int j;
+            for (j = i; j >= gap && my_arr[j - gap] > temp; j -= gap)
+            my_arr[j] = my_arr[j - gap];
+            my_arr[j] = temp;
+         }
+      }
+      return 0;
+   }
+   public static void main(String args[]) {
+      int my_arr[] = { 12, 34, 54, 2, 3 };
+      Demo my_instance = new Demo();
+      my_instance.shell_sort(my_arr);
+      System.out.println("The array, after performing shell sort is : ");
+      int arr_len = my_arr.length;
+      for (int i = 0; i < arr_len; ++i)
+      System.out.print(my_arr[i] + " ");
+      System.out.println();
+   }
+}
 shellsort(arr, num);    
 System.out.println("\n Sorted array is: ");    
 for (k = 0; k<num; k++)    
