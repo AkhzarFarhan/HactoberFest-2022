8be55fdd43ec2575da66b453df995b2f3fe7e03f
ins-arun <60340001+ins-arun@users.noreply.github.com>
2021-10-26 22:16:53 +0530

    Update Factorial.java

--- Java/Factorial.java
+++ Java/Factorial.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-public class Factorial {
-
+public class factorial {
 	public static void main(String[] args) {
+		int number;
+		System.out.println("Java program to find factorial of a number");
+		//read the number using scanner class
+		System.out.println("Enter a number to find its factorial:");
+		Scanner stdin=new Scanner(System.in);
+		number=stdin.nextInt();
+		long  factorial=1;
+		//finds factorial
+		for(int i=1; i<=number ;  i++)
+		{
+			factorial=factorial*i;
+		}
+		System.out.println("Factorial of "+number+" is "+factorial);
+	}
 
-
-
-Scanner sc =new Scanner(System.in);
-
-int n=sc.nextInt();
-
-int factorial=1;
-for(int i=n;i>=1;i--){
-  factorial=factorial * i;
-  }
-  system.out.println(factorial)
-  
-  }
-  }
+}
